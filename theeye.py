2cf92f3d7f87d39127bda4aea61caf833013d78c
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2020-07-24 14:01:03 -0300

    Update

--- theeye.py
+++ theeye.py
@@ -105,7 +105,7 @@ def main():
             if len(output_information) > 15:
                 slack_data = {'text': 'Ports {discovered_ports} open on: {ip_target} {output_information}'.format(discovered_ports=discovered_ports, ip_target=ip_target, output_information=output_information)}
             else:
-                slack_data = {'text': '{output_information} open on: {ip_target}'.format(discovered_ports=discovered_ports, ip_target=ip_target, output_information=output_information)}
+                slack_data = {'text': '{output_information} on: {ip_target}'.format(discovered_ports=discovered_ports, ip_target=ip_target, output_information=output_information)}
                 
             requests.post(webhook, data=json.dumps(slack_data), headers={'Content-Type': 'application/json'})
         except Exception as e:
